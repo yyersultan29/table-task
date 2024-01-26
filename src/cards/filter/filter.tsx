2ca5c2d74349d0b1c5dbc8cb53c4e59dbecf2f9d
yersultan <180103230@stu.sdu.edu.kz>
2024-01-26 19:16:44 +0600

    fix: useSearch params

--- src/cards/filter/filter.tsx
+++ src/cards/filter/filter.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback, useState } from "react"
+import { ChangeEvent, useCallback, useEffect, useLayoutEffect, useState } from "react"
 
 import { Price } from "./components/price"
 import { HouseType } from "./components/house-type"
@@ -6,28 +6,34 @@ import { RatingStar } from "./components/rating-star"
 import { CountryList } from "./components/country-list"
 import { ReviewCount } from "./components/review-count"
 import { Button } from "../../components/button/button"
+import { useSearchParams } from "react-router-dom"
 
 export const Filter = () => {
 
+  const [params, setParams] = useSearchParams();
+
   const [selectedCountries, setSelectedCountries] = useState<string[]>([]);
-
   const [type, setType] = useState<string[]>([]);
-
   const [stars, setStars] = useState<string[]>([]);
-
   const [reviewCount, setReviewCount] = useState(0);
-
   const [from, setFrom] = useState(0);
   const [to, setTo] = useState(0);
 
-  const handleChangeFrom = useCallback((e: ChangeEvent<HTMLInputElement>) => setFrom(Number(e.target.value)), []);
+  const handleChangeFrom = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setFrom(Number(e.target.value));
 
-  const handleChangeTo = useCallback((e: ChangeEvent<HTMLInputElement>) => setTo(Number(e.target.value)), []);
+  }, []);
+
+  const handleChangeTo = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setTo(Number(e.target.value))
+
+  }, []);
 
   const handleChangeReviewCount = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     if (isNaN(Number(e.target.value))) return;
 
-    setReviewCount(Number(e.target.value))
+    setReviewCount(Number(e.target.value));
+
   }, [])
 
   const handleChangeStars = useCallback((e: ChangeEvent<HTMLInputElement>) => {
@@ -36,6 +42,7 @@ export const Filter = () => {
     } else {
       setStars(prev => prev.filter(el => el !== e.target.value));
     }
+
   }, [])
 
   const handleChangeType = useCallback((e: ChangeEvent<HTMLInputElement>) => {
@@ -44,6 +51,7 @@ export const Filter = () => {
     } else {
       setType(prev => prev.filter(el => el !== e.target.value));
     }
+
   }, [])
 
   const handleSelectCountry = useCallback((e: ChangeEvent<HTMLInputElement>) => {
@@ -53,7 +61,37 @@ export const Filter = () => {
     } else {
       setSelectedCountries(prev => prev.filter(el => el !== e.target.value.trim()));
     }
-  }, [])
+
+  }, []);
+
+  useLayoutEffect(() => {
+    params.has("country") && setSelectedCountries(JSON.parse(params.get("country") ?? ""));
+    params.has("type") && setType(JSON.parse(params.get("type") ?? ""));
+    params.has("stars") && setStars(JSON.parse(params.get("stars") ?? ""));
+    params.has("review") && setReviewCount(Number(params.get("stars")));
+    if (params.has("from") && params.has("to")) {
+      setFrom(Number(params.get("from")));
+      setTo(Number(params.get("to")));
+    }
+  }, []);
+
+  useEffect(() => {
+    selectedCountries.length ? params.set("country", JSON.stringify(selectedCountries)) : params.delete("country");
+    type.length ? params.set("type", JSON.stringify(type)) : params.delete("type");
+    stars.length ? params.set("stars", JSON.stringify(stars)) : params.delete("stars");
+    reviewCount ? params.set("review", reviewCount.toString()) : params.delete("review");
+    if (to && !from) {
+      params.set("price_from", "0")
+      params.set("price_to", to.toString());
+    } if (!to && from) {
+      params.set("price_from", from.toString());
+      params.set("price_to", "500000");
+    }
+    setParams(params);
+
+  }, [selectedCountries, type, stars, reviewCount, from, to]);
+
+  console.log("Render")
 
   return (
     <div className="w-[325px] flex flex-col gap-[25px]">
