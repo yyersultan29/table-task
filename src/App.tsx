1f9e14dff88a27126128fcad52ea572e57f74be0
yyersultan29 <yersultanyerkinbek01@gmail.com>
2024-07-15 17:08:58 +0500

    workflow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useRef } from "react";
 import "./App.scss";
-import { load } from "@2gis/mapgl";
 // import { useEffect } from "react";
-import Cookeis from "js-cookie";
 import { YMap, YMapsProvider } from "re-ymaps";
 
 // const getRandomList = () => new Array(10_000).fill(0).map((_,index) => Math.random());
